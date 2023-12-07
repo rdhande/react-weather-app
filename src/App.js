46d920ddc1ffef75e2507dc474c7424fe01ba606
Rashi Milind Dhande <31810647+rdhande@users.noreply.github.com>
2023-12-07 19:21:23 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Weather App
+        <footer>
+          <a
+            href="https://github.com/rdhande/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          , by <a href="https://github.com/rdhande">Rashi</a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
